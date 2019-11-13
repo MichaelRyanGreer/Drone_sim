Fixed rotation problem

forces_test.py
@@ -13,8 +13,8 @@ matplotlib.interactive(True)
 pi = math.radians(180)
 
 # state
-a       = [0,0,pi/4]
-adot    = [0,1,1]
+a       = [0,pi/2,0]
+adot    = [1,1,1]
 p       = [0,0,10]
 pdot    = [0,0,0]
 
@@ -59,7 +59,7 @@ while(1):
 
     a, adot, p, pdot = myplane.getX()
 
-    rot_axis = adot + p
+    rot_axis = np.dot(myplane.getRot(), np.transpose(adot)) + p
 
     axis = ax.scatter(rot_axis[0],rot_axis[1],rot_axis[2],color='g')
 

plane.py
@@ -43,13 +43,19 @@ class Plane:
         self.position		= np.array(position)
         self.position_dot	= np.array(position_dot)
 
+        self.gen_rot()
+
     def getX(self):
 
         return self.attitude, self.attitude_dot, self.position, self.position_dot
 
+    def getRot(self):
+
+        return self.rot_mat
+
     def getPoints(self):
 
-        self.gen_rot()
+        #self.gen_rot()
 
         offset = np.transpose(np.tile(self.position, (len(self.base_points[0]), 1)))
 
@@ -60,12 +66,8 @@ class Plane:
 
     def physics_step(self, t_step=0.1):
 
-        # Generate current rotation matrix
-        self.gen_rot()
-
         # Position step
-        #F = self.getForces()
-        F = np.array([0,0,0])
+        F = self.getForces()
 
         dx = self.position_dot * t_step
 
@@ -80,11 +82,11 @@ class Plane:
         # Rotation step
         T = self.getTorques()
 
-        dt = self.attitude_dot * t_step
+        dt = np.cross(self.attitude_dot,self.rot_mat) * t_step
 
         dw = (T/self.inertia)*t_step
 
-        self.attitude = self.attitude + dt
+        self.rot_mat = self.rot_mat + dt
 
         self.attitude_dot = self.attitude_dot + dw
 